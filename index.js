a6e535775b1bc8e8a4daa2a66abdfedd9d48cf69
Marcos Aleixo <maknytz@gmail.com>
2025-12-30 14:13:22 -0300

    Inserindo o public path e o set session

--- index.js
+++ index.js
@@ -44,6 +44,17 @@ app.use(
 // flash messages
 app.use(flash())
 
+// public path
+app.use(express.static('public'))
+
+// set session to res
+app.use((req, res, next) => {
+    if (req.session.userid) {
+        res.locals.session = req.session
+    }
+    next()
+})
+
 conn
 .sync()
 .then(() => {
