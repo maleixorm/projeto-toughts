85c65ace390aa30173bb1329defb42f8b0f51664
Marcos Aleixo <maknytz@gmail.com>
2026-01-03 20:09:59 -0300

    Criando o método logout no controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -49,4 +49,9 @@ module.exports = class AuthController {
             console.log(error)
         }
     }
+
+    static logout(req, res) {
+        req.session.destroy()
+        res.redirect('/')
+    }
 }
