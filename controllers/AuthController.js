90ae79863c515d05b788f64c203fec03e080f416
Marcos Aleixo <maknytz@gmail.com>
2026-01-03 19:45:12 -0300

    Validando se o usuário existe

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -13,8 +13,17 @@ module.exports = class AuthController {
     static async registerPost(req, res) {
         const { name, email, password, confirmpassword } = req.body
 
+        // password match validation
         if (password != confirmpassword) {
             req.flash('message', 'As senhas não conferem! Tente novamente!')
+            res.render('auth/register')
+            return
+        }
+
+        // check if user exists
+        const checkIfUserExists = await User.findOne({ where: {email: email} })
+        if (checkIfUserExists) {
+            req.flash('message', 'O email já está em uso! Tente novamente!')
             res.render('auth/register')
             return
         }
