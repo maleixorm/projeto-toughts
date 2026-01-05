1d27d4141bd124da9064c49a3716977df1648ff6
Marcos Aleixo <maknytz@gmail.com>
2026-01-04 23:15:58 -0300

    Criando o método de login e validações

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,6 +5,33 @@ module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login')
     }
+
+    static async loginPost(req, res) {
+        const {email, password} = req.body
+
+        // find user
+        const user = await User.findOne({where: {email: email}})
+        if (!user) {
+            req.flash('message', 'Usuário não encontrado!')
+            res.render('auth/register')
+            return
+        }
+
+        // check if password match
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch) {
+            req.flash('message', 'Senha inválida!')
+            res.render('auth/login')
+            return
+        }
+
+        req.session.userid = user.id
+        req.flash('message', 'Login realizado com sucesso!')
+        req.session.save(() => {
+            res.redirect('/')
+        })
+    }
     
     static register(req, res) {
         res.render('auth/register')
@@ -52,6 +79,6 @@ module.exports = class AuthController {
 
     static logout(req, res) {
         req.session.destroy()
-        res.redirect('/')
+        res.redirect('/login')
     }
 }
