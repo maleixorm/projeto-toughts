0dbe262713c1e5cad01cf08590dbf4b2c7b129ba
Marcos Aleixo <maknytz@gmail.com>
2026-01-05 21:52:48 -0300

    Inserindo o middleware de verificação na rota dashboard

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const ToughtController = require('../controllers/ToughtController')
+const checkAuth = require('../helpers/auth').checkAuth
 
-router.get('/dashboard', ToughtController.dashboard)
+router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.get('/', ToughtController.showToughts)
 
 module.exports = router
